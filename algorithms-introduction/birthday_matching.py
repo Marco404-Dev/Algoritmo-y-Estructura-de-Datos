cf79cd7ef83cfa698766e9e06b2e6fec747aa41e
Marco404 <anccoquispemarco@gmail.com>
2025-12-16 22:02:28 -0500

    Update birthday_matching.py

--- algorithms-introduction/birthday_matching.py
+++ algorithms-introduction/birthday_matching.py
@@ -1,22 +1,26 @@
 def birthday_match(students):
-    """
-    students: lista de tuplas (name, bday)
-    bday puede ser un número 1..365 o (mes, dia), etc.
-    retorna (nameA, nameB) o None
-    """
     n = len(students)
-    record = [None] * n  # imita StaticArray(n)
+    record = [None] * n
 
     for k in range(n):
         name1, bday1 = students[k]
 
-        # buscar si bday1 ya aparece en record[0..k-1]
         for i in range(k):
             name2, bday2 = record[i]
             if bday1 == bday2:
                 return (name1, name2)
 
-        # guardar el estudiante actual
         record[k] = (name1, bday1)
 
     return None
+
+
+# estudiantes
+students = [
+    ("Ana", (3, 10)),
+    ("Luis", (7, 22)),
+    ("Maria", (3, 10)),
+]
+# print
+print(birthday_match(students))   
+
