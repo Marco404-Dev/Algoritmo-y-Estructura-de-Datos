98133c5d42c4999ffd936735a31b0b84162bf6e9
Marco404 <anccoquispemarco@gmail.com>
2026-01-01 17:09:20 -0500

    Update birthday_matching - 2 solucion.py

--- Clases/1-introduccion-algoritmos/birthday_matching - 2 solucion.py
+++ Clases/1-introduccion-algoritmos/birthday_matching - 2 solucion.py
@@ -15,6 +15,24 @@ def coincidencia_cumpleanos_dic(estudiantes):
     
     return None
 
+# --- Lista de ejemplo ---
+estudiantes = [
+    ("Ana", "01-01"),
+    ("Luis", "12-05"),
+    ("Ruth", "01-01"),  # misma fecha que Ana
+    ("Carlos", "23-08")
+]
+
+# --- Llamar a la función ---
+resultado = coincidencia_cumpleanos_dic(estudiantes)
+
+if resultado:
+    print(f"Coincidencia encontrada: {resultado[0]} y {resultado[1]}")
+else:
+    print("No hay coincidencias de cumpleaños")
+
+
+
 ## Tiempo: O(n)
 
 ## Espacio: O(n)
